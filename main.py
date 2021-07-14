1f7a2c14e3b1e24e705f5d2ee8f721e7757e15dc
Toby Sullivan <toby@streak.com>
2021-07-14 11:49:10 -0700

    Implement concurrent sending

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 import math
 import os
 import random
+import threading
 import time
 
 from googleapiclient.discovery import build
@@ -18,11 +19,17 @@ SCOPES = [
         'https://www.googleapis.com/auth/gmail.readonly',
         'https://www.googleapis.com/auth/gmail.send',
         ]
+BATCH_SIZE = 5
 timestamp_fmt = "%Y-%m-%d %H:%M:%S.%f"
 
 log_file = "send_log.csv"
 creds_file = "credentials.json"
 token_file = "token.json"
+
+backing_off = False
+backing_off_lock = threading.Lock()
+gmail_lock = threading.Lock()
+log_lock = threading.Lock()
 
 def send_message(gmail, from_addr, to, subject, body):
         message = generate_message(from_addr, to, subject, body)
@@ -49,9 +56,10 @@ def generate_message(from_addr, to_addr, subject, body):
 
 def log_result(pid, trial, time, status, address, subject, body):
     fmt_time = time.strftime(timestamp_fmt)
-    with open(log_file, 'a') as csvfile:
-        logwriter = csv.writer(csvfile)
-        logwriter.writerow([pid, trial, fmt_time, status, address, subject, body])
+    with log_lock:
+        with open(log_file, 'a') as csvfile:
+            logwriter = csv.writer(csvfile)
+            logwriter.writerow([pid, trial, fmt_time, status, address, subject, body])
 
 def get_last_trial():
     last_trial = 0
@@ -79,46 +87,72 @@ def load_google_creds():
     return creds
 
 def build_gmail_client():
-    creds = load_google_creds()
-    service = build('gmail', 'v1', credentials=creds)
-    return service
+    with gmail_lock:
+        creds = load_google_creds()
+        service = build('gmail', 'v1', credentials=creds)
+        return service
+
+def run_trial(pid, from_addr, trial):
+    now = datetime.utcnow()
+    email = f'test{trial}@example.com'
+    subject = f'Test email {trial}'
+    body = f'This is test email #{trial}'
+
+    with build_gmail_client() as gmail:
+        status = send_message(gmail, from_addr, email, subject, body)
+
+    log_result(pid, trial, now, status, email, subject, body)
+    if status == 200:
+        stop_backoff()
+        print(f'{trial}: Message sent successfully')
+    else:
+        start_backoff()
+        print(f'{trial}: Message send failed.')
+
+def start_backoff():
+    global backing_off
+    with backing_off_lock:
+        backing_off = True
+
+def stop_backoff():
+    global backing_off
+    with backing_off_lock:
+        backing_off = False
 
 def main():
     from_addr = os.environ.get(ENV_FROM_ADDR)
     if not from_addr:
         raise Exception(f'Missing required environment variable: {ENV_FROM_ADDR}')
 
-    gmail = None
-    last_gmail_refresh = None
-
     pid = os.getpid()
     n = get_last_trial() + 1
     retries = 0
     while True:
-        if not gmail or (datetime.utcnow() - last_gmail_refresh).total_seconds() > 600:
-            if gmail:
-                gmail.close()
-            gmail = build_gmail_client()
-            last_gmail_refresh = datetime.utcnow()
+        if backing_off:
+            trial = n
+            n += 1
 
-        trial = n
-        n += 1
-        now = datetime.utcnow()
-        email = f'test{trial}@example.com'
-        subject = f'Test email {trial}'
-        body = f'This is test email #{trial}'
-
-        status = send_message(gmail, from_addr, email, subject, body)
-        log_result(pid, trial, now, status, email, subject, body)
-        if status == 200:
-            retries = 0
-            print(f'{trial}: Message sent successfully')
-        else:
-            retries += 1
+            # Sleep for a short while then send a single message
             sleep_time = round(0.2 * 1.5 ** retries, 3) # 200ms * 1.5^retries
             sleep_time = min(sleep_time, 60) # Cap to 60 seconds
-            print(f'{trial}: Message send failed. Sleeping for {sleep_time} seconds...')
+            print(f'Sleeping for {sleep_time} seconds...')
             time.sleep(sleep_time)
+            run_trial(pid, from_addr, trial)
+            retries += 1
+        else:
+            # Send a small batch of messages concurrently
+            retries = 0
+            threads = []
+            for i in range(BATCH_SIZE):
+                trial = n
+                n += 1
+
+                thread = threading.Thread(target=run_trial, args=[pid, from_addr, trial])
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
 
     print("Done")
 
